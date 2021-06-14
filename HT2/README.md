01ed29b2982d917b4c88f3f572add00ab68d36b2
Mikalai Rumiantsau <mikalairumiantsau@gmail.com>
2021-06-14 16:35:38 +0300

    Finished task #2

---
+++ HT2/README.md
@@ -0,0 +1,20 @@
+# HT2 implementation
+
+File script.sh contains result of transformation task.
+
+Task definition:
+sudo netstat -tunapl | awk '/firefox/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
+
+Result:
+
+1. sudo netstat -tunapl - list all open ports
+2. awk '/firefox/ {print $5}' - port filter by process name
+3. cut -d: -f1 - we leave only ip-addresses, without an open port number
+4. sort - sorting ip addresses
+5. uniq -c - counting the number of unique ip
+6. sort - sorting ip addresses
+7. tail -n5 - limiting the number of displayed lines of ip-addresses
+8. grep -oP '(\d+\.){3}\d+' - output by mask (regular expression) of only ip-addresses
+9. "while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done" - whois command for each ip address in the loop
+
+For the implementation of all tasks, the initial data were checked, the input of information from the keyboard was checked, the user was checked from which the script was launched. userfriendly or displaying output. One additional task was done (number 2 additional information with whois).
--- HT2/script.sh
+++ HT2/script.sh
@@ -1,26 +1,49 @@
-
 #!/bin/bash
 set -e # Exit immediately if a simple command exits with a non-zero status
-
 #set -x # Activate debugging from here
 
+# Key for netstat command
+ netStatPar="-tunapl"
+
+# Checking script run privileges root / non root
+if  [ $(id -u) = 0 ]
+ then
+        echo ====================================================
+        echo "-----------The Script is run as root!-------------"
+        echo ====================================================
+else
+
+        echo ====================================================
+        echo "------The script is run without root rights!------"
+        echo ====================================================
+fi
+
+# Check for empty input
 function empty_input {
 if [ -z "$processName" ]
   then
-    echo "error: No argument supplied. Script will be finish"
+        echo ====================================================
+        echo "error: No argument supplied. Script will be finish"
+        echo ====================================================
     exit 1
 fi
 }
 
+# Check for numeric input
 function number_input {
 re='^[0-9]+$'
 if ! [[ $quLines =~ $re ]] ; then
-  echo "error: Not a number. Script will be finish" >&2; show_connects_stat=0; exit 1
+        echo ====================================================
+        echo "----error: Not a number. Script will be finish----" >&2; show_connects_stat=0; exit 1
+        echo ====================================================
 fi
 }
 
-function show_without_columns {
-netstat -tunapl |
+#
+function scriptRun {
+        echo ====================================================
+        echo "-------------Output script result-----------------"
+netstat "$netStatPar" 2>/dev/null |
 awk '/'$processName'/ ''{print $5}'|
 cut -d: -f1 |
 sort |
@@ -30,23 +53,54 @@ tail -n"$quLines" |
 grep -oP '(\d+\.){3}\d+' |
 while read IP ; do whois $IP |
 awk -F':' '/^Organization/ {print $2}'; done
+        echo "------------------Script end----------------------"
+        echo ====================================================
 exit 1
 }
 
-function show_with_columns {
-echo "___________Table with other second informations----------"
-netstat -tunapl | awk '/'$processName'/ ''{print $5 "        " $6}'
-#clear
-#echo "-----------Table with other second information-----------"
-netstat -tunapl | awk '/'$processName'/ ''{print $5}'|
+# Command execution without outputting additional data
+function show_without_columns {
+read -p "Show other columns in whois output? (Y/N): " confirm && [[ $confirm == [yY] ||
+ $confirm == [yY][eE][sS] ]] || scriptRun
+echo "Input additional column name for whois"
+read addInWhois
+netstat "$netStatPar" 2>/dev/null |
+awk '/'$processName'/ ''{print $5}'|
 cut -d: -f1 |
 sort |
 uniq -c |
 sort |
 tail -n"$quLines" |
 grep -oP '(\d+\.){3}\d+' |
-while read IP; do whois $IP |
-awk -F':' '/^Organization/ {print $2}'; done
+while read IP ; do whois $IP |
+awk -F':' '/^Organization/ || ''/'$addInWhois'/ ''{print $1 $2}'; done
+        echo "------------------Script end---------------------"
+        echo ===================================================
+exit 1
+}
+
+# Сommand execution with additional data output
+function show_with_columns {
+        echo ===================================================
+        echo "-------Table with other second informations------"
+        echo ===================================================
+netstat "$netStatPar" 2>/dev/null | awk '/'$processName'/ ''{print $5 "        " $6}'
+read -p "Show other columns in whois output? (Y/N): " confirm && [[ $confirm == [yY] ||
+ $confirm == [yY][eE][sS] ]] || scriptRun
+echo "Input additional column name for whois"
+read addInWhois
+netstat "$netStatPar" 2>/dev/null |
+awk '/'$processName'/ ''{print $5}'|
+cut -d: -f1 |
+sort |
+uniq -c |
+sort |
+tail -n"$quLines" |
+grep -oP '(\d+\.){3}\d+' |
+while read IP ; do whois $IP |
+awk -F':' '/^Organization/ || ''/'$addInWhois'/ ''{print $1 $2}'; done
+        echo "-----------------Script end----------------------"
+        echo ===================================================
 exit 1
 }
 
@@ -60,7 +114,7 @@ read quLines
 
 number_input
 
+# Processing a request to display additional information (Yes/No)
 read -p "Show other columns state? (Y/N): " confirm && [[ $confirm == [yY] ||
  $confirm == [yY][eE][sS] ]] || show_without_columns
-
 show_with_columns
