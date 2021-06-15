a36500a5a6cc865011a4b13ff12e78439e12b395
Mikalai Rumiantsau <mikalairumiantsau@gmail.com>
2021-06-15 15:04:47 +0300

    update py-app #2

--- HT1/app.py
+++ HT1/app.py
@@ -5,3 +5,14 @@ from marshmallow import Schema, fields, ValidationError
 
 #TODO: make config file for service
 sign = 'Made with ❤️ by MikalaiRumiantsau\n'
+
+def findEmoji(text):
+    r = emoji.emojize(':' + text + ':')
+    return(r.replace(':',''))
+
+class BaseSchema(Schema):
+      animal = fields.String(required=True)
+      sound = fields.String(required=True)
+      count = fields.Integer(required=True)
+
+app = Flask(__name__)
