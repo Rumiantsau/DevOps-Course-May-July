8f2980dca10f368d3d71aac2fef6ba1501222dc6
Mikalai Rumiantsau <mikalairumiantsau@gmail.com>
2021-06-15 15:05:45 +0300

    update py-app #3

--- HT1/app.py
+++ HT1/app.py
@@ -16,3 +16,33 @@ class BaseSchema(Schema):
       count = fields.Integer(required=True)
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    #TODO: make somethink to work with GET
+    if request.method == "GET":
+        return 'Your method is GET\n'
+    #TODO: make somethink to work with POST
+    if request.method == "POST":
+        data = request.get_json(force=True)
+        schema = BaseSchema()
+        try:
+            #TODO: validation of entered data
+            result = schema.load(data)
+            if int(data['count']) < 0:
+                return (sign)
+        except ValidationError as err:
+            return jsonify(err.messages),400
+        # TODO: formation and output of the received data
+        str = ''
+        n=int(data['count'])
+        emoji = findEmoji(data['animal'])
+        if emoji == data['animal']:
+            return '\n The emoji for input animal does not exist \n'
+        for i in range (0, n):
+          str += '{animal} says {sound}\n'.format(animal=findEmoji(data['animal']), sound=data['sound'])
+    return (str+sign),200
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
