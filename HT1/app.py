a9606774b0b6773848c05973ada669bafe737e5d
Mikalai Rumiantsau <mikalairumiantsau@gmail.com>
2021-06-22 23:44:32 +0300

    1. updated app run command 2. updated ssh 3. updated gitignore

--- HT1/app.py
+++ HT1/app.py
@@ -45,4 +45,4 @@ def index():
     return (str+sign),200
 
 if __name__ == "__main__":
-   server(app, host='0.0.0.0', port=5000, url_scheme='https')
+   serve(app, host='0.0.0.0', port=5000, url_scheme='https')
